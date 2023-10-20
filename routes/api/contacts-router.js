970e10ebd10e6c513708c9f0d2360138ca38dd45
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-10-20 11:57:11 +0300

    done v1.0.0 basic version

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isEmptyBody, isIdChecker, isEmptyBodyFavorite } from '../../middlewares/index.js'
+import { isEmptyBody, isIdChecker, isEmptyBodyFavorite, authorization } from '../../middlewares/index.js'
 import { contactsSchema, contactsSchemaFavorite } from '../../models/Model-contacts.js'
 import { validateBodyReq } from '../../decorators/index.js'
 import contactsControllers from '../../controllers/contact-controller.js'
@@ -8,17 +8,18 @@ const contactsRouter = express.Router()
 const ValidatorContacts = validateBodyReq(contactsSchema)
 const ValidatorContactsFavorite = validateBodyReq(contactsSchemaFavorite)
 
-contactsRouter.get('/', contactsControllers.getAll)
+contactsRouter.get('/', authorization, contactsControllers.getAll)
 
-contactsRouter.get('/:contactId', isIdChecker, contactsControllers.getById)
+contactsRouter.get('/:contactId', authorization, isIdChecker, contactsControllers.getById)
 
-contactsRouter.post('/', isEmptyBody, ValidatorContacts, contactsControllers.addNew)
+contactsRouter.post('/', authorization, isEmptyBody, ValidatorContacts, contactsControllers.addNew)
 
-contactsRouter.put('/:contactId', isEmptyBody, isIdChecker, ValidatorContacts, contactsControllers.updateById);
+contactsRouter.put('/:contactId', authorization, isEmptyBody, isIdChecker, ValidatorContacts, contactsControllers.updateById);
 
-contactsRouter.patch('/:contactId/favorite', isIdChecker, isEmptyBodyFavorite, ValidatorContactsFavorite, contactsControllers.updateByIdFavorite);
+contactsRouter.patch('/:contactId/favorite', authorization, isIdChecker, isEmptyBodyFavorite, ValidatorContactsFavorite, contactsControllers.updateByIdFavorite);
+contactsRouter.patch('/:contactId/favorite', authorization, isIdChecker, isEmptyBodyFavorite, ValidatorContactsFavorite, contactsControllers.updateByIdFavorite);
 
-contactsRouter.delete('/:contactId', isIdChecker, contactsControllers.deleteById)
+contactsRouter.delete('/:contactId', authorization, isIdChecker, contactsControllers.deleteById)
 
 
 export default contactsRouter;
